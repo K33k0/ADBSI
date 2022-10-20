5ae771e81c3c124ddfd7d4513b104a3217a492eb
Kieran Wynne <keewynne90@gmail.com>
2022-10-20 20:47:28 +0100

    Code reformat

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import os
 from pytermgui.enums import HorizontalAlignment, VerticalAlignment
 import toml
 
+
 def kill_app(*args):
     sys.exit()
+
 
 def run(stage):
     status = None
@@ -18,34 +20,37 @@ def run(stage):
     # Loop through each device running the selected file
     for s in get_all_devices():
         # Loop through each file in the config and sideload the file that matches the button pressed
-        for name, file in config['FileLocation'].items():
+        for name, file in config["FileLocation"].items():
             if name == stage.label:
                 status = subprocess.Popen(
-                    [config['adbLocation'],'-s',s,'sideload',file],
+                    [config["adbLocation"], "-s", s, "sideload", file],
                     stdout=subprocess.PIPE,
                     universal_newlines=True,
-                    stderr=subprocess.STDOUT
-                    )
-    
-# Loop through the terminal output from the adb command
+                    stderr=subprocess.STDOUT,
+                )
+
+    # Loop through the terminal output from the adb command
     while True:
         try:
             nextline = status.stdout.readline()
         except Exception:
-            print('Failed at readline')
+            print("Failed at readline")
             raise
         # if line of output is empty or adb is running then kill it
-        if nextline == '' and status.poll() is not None:
+        if nextline == "" and status.poll() is not None:
             status.kill()
             break
         # parse out the number numbers from within the adb output. Turning it into a value out of 100
-        if '(' in nextline:
-            current_status = float(nextline[nextline.find("(")+1:nextline.find(")")][1:-1])
+        if "(" in nextline:
+            current_status = float(
+                nextline[nextline.find("(") + 1 : nextline.find(")")][1:-1]
+            )
             status_text.value = f"{int(current_status)}/100"
-            
+
     exitcode = status.returncode
     if exitcode == 0:
-        status_text.value = (f"{stage.label} Complete")
+        status_text.value = f"{stage.label} Complete"
+
 
 def reboot_recovery(button):
     devices = get_all_devices()
@@ -55,32 +60,40 @@ def reboot_recovery(button):
 
     for s in devices:
         status_text.value = "Rebooting to recovery"
-        status = subprocess.Popen([config['adbLocation'],'-s',s,'reboot','recovery'],stdout=subprocess.PIPE,universal_newlines=True,stderr=subprocess.STDOUT)
+        status = subprocess.Popen(
+            [config["adbLocation"], "-s", s, "reboot", "recovery"],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+            stderr=subprocess.STDOUT,
+        )
         while True:
             nextline = status.stdout.readline()
-            if nextline == '' and status.poll() is not None:
+            if nextline == "" and status.poll() is not None:
                 break
         status.communicate()[0]
         status_text.value = "Completed reboot recovery"
 
+
 def get_all_devices(button=None):
     serial_numbers = []
-    status = check_output([config['adbLocation'],'devices']).decode('utf-8').split('\n')[1:]
+    status = (
+        check_output([config["adbLocation"], "devices"]).decode("utf-8").split("\n")[1:]
+    )
     for l in status:
-        if 'device' in l or 'sideload' in l:
+        if "device" in l or "sideload" in l:
             serial_numbers.append(l[:14])
     if button:
         status_text.value = f"{len(serial_numbers)} connected"
     else:
         return serial_numbers
- 
 
-config = toml.load('config.toml')
+
+config = toml.load("config.toml")
 with ptg.WindowManager() as manager:
     exit_button = ptg.Button("Exit", kill_app)
     # Create a list of buttons
-    my_buttons = [ptg.Button(name, run) for name in config['FileLocation'].keys()]
-    get_devices_button = ptg.Button("Get Serial Numbers",get_all_devices)
+    my_buttons = [ptg.Button(name, run) for name in config["FileLocation"].keys()]
+    get_devices_button = ptg.Button("Get Serial Numbers", get_all_devices)
     recover_button = ptg.Button("Reboot recovery", reboot_recovery)
     status_text = ptg.Label()
     status_container = ptg.Container(status_text)
@@ -91,7 +104,7 @@ with ptg.WindowManager() as manager:
         get_devices_button,
         recover_button,
         exit_button,
-        status_container
+        status_container,
     )
     manager.add(window)
     manager.run()
